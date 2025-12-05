7c360a849bf9434300a540cadec2ac4bc227ba4a
abhishekdixit4112 <abhishekdixit4112@gmail.com>
2025-12-05 23:14:18 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ const app=require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log("server is running on http://localhost:3000");
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
